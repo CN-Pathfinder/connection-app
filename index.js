6740e6fb49cbe306d448343ea7f7b5363b7b75ef
Lucy <lucyhelendonnelly@gmail.com>
2020-07-02 13:50:25 +0100

    added routes and model

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const app = express();
+const path = require('path')
+
+app.use(express.urlencoded({extended: true}));
+// parses the url-encoded bodies(that are sent by html forms)
+
+app.use(express.json());
+// parses the json bodies(that are sent by api clients)
 
 app.get('/about', (req, res) => {
     res.send({ someText: "hello" });
@@ -7,7 +14,8 @@ app.get('/about', (req, res) => {
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
-    const path = require('path'); app.get('*', (req, res) => {
+
+    app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
 }
---
+++ routes/registerrouter.js
@@ -0,0 +1,26 @@
+const { Router } = require('express'); //like in react, need to get this bit from express
+const router = Router();
+const User = require('../models/User')
+const siteUser = require('../models/User')
+
+
+router.get('/register', async(req, res)=>{
+    const newUser = new User(req.body);
+    // await newUser.save();
+
+    await newUser.save(function(err){
+        if (err){
+            console.log(error)
+        } else{
+            res.render("/registered")
+        }
+    })
+// if there are errors during the save, then console.log "error", but if they weren't, then render the registered page. 
+
+if (await siteUser.validateSignup(name, email)){
+    res.render('platform', {err: 'A user with that username already exists - please choose another!'});
+    return; 
+    
+}
+
+})
