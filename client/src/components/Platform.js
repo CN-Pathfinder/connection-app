4070564ff6c4279c86cc738f9e236d5816411674
Lucy <lucyhelendonnelly@gmail.com>
2020-07-03 11:59:45 +0100

    registration form functions

--- client/src/components/Platform.js
+++ client/src/components/Platform.js
@@ -7,15 +7,20 @@ class Platform extends React.Component {
     return (
       <div>
           <div id="platformWrapper">
+
             <div id="PlatformNav"><PlatformNav /></div>
+
             <div id="locationdiv">
+
               <div id="locationtext">
                 Choose your location to see registered users in that area.<br/>
                 You can filter by if you are looking for help or if you require help.<br/>
                 You can then look at a users profile to see more details about their circumstances.<br/>
                 If you have found a user that may suit your needs then click the message button to get in contact with them.
                 </div>
+
                 <br/> 
+
               <div id="locationdropdown">
                 <label for="location">Location:</label>
                 <select name="location" id="location">
@@ -23,9 +28,10 @@ class Platform extends React.Component {
                   <option value="chester">Chester</option>
                   <option value="luton">Luton</option>
                   <option value="bradford">Bradford</option>
-                </select></div>
+                </select></div> 
+
                 <div id="locationcheckbox">
-                <form action="/action_page.php">
+                <form>
                   <div>Please choose one or both from the options below:</div><br/>
               
                   <input type="checkbox" id="helpee" name="helpee" value="helpee"></input>
