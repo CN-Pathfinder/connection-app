884fb9feaa0f22c8c0cbe204311a405999da3a33
Richard Wardingley <richardwardingley@hotmail.com>
2020-07-07 16:54:22 +0100

    updated messages

--- client/src/components/Messages.js
+++ client/src/components/Messages.js
@@ -9,10 +9,20 @@ class Messages extends React.Component {
         read: false,
     }
 
+    handleSubmit = async (event) => {
+        event.preventDefault();
+        console.log("Hello World");
+    }
+
     render(){
         return (
             <div id="messages">
                 Hello LucyHD (coming soon in 16k)
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor="messageTest">Input message here:</label>
+                    <input type="text" name="messageTest" />
+                    <button type="submit">Send message</button>
+                </form>
             </div>
         )
     }
--- routes/messagerouter.js
+++ routes/messagerouter.js
@@ -5,7 +5,13 @@ const User = require('../models/User');
 
 router.get('/', (req, res) => {
     return res.status(200).json({
-        message: 'Hello... testing'
+        message: 'Testing get'
+    })
+})
+
+router.post('/', async (req, res) => {
+    return res.status(200).json({
+        message: 'Testing post'
     })
 })
 
