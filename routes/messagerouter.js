9ddca500cfd8b56d195849c687dced15acb4abaf
Richard Wardingley <richardwardingley@hotmail.com>
2020-07-08 16:52:06 +0100

    message front end form, messagerouter post and save, and started get method

--- routes/messagerouter.js
+++ routes/messagerouter.js
@@ -2,17 +2,30 @@ const express = require('express');
 const { Router } = require('express');
 const router = Router();
 const User = require('../models/User');
+const Message = require('../models/Message');
 
-router.get('/', (req, res) => {
+
+router.get('/', async (req, res) => {
     return res.status(200).json({
-        message: 'Testing get'
+        message: 'Testing get',
+        data: await Message.find()
     })
 })
 
 router.post('/', async (req, res) => {
+    const message = new Message({
+        sender: req.body.sender,
+        recipient: req.body.recipient,
+        dateTime: req.body.dateTime,
+        messageBody: req.body.messageBody,
+        read: req.body.read,
+        });
+    message.save()
     return res.status(200).json({
         message: 'Testing post'
     })
 })
+
+
 
 module.exports = router;
